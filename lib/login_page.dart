3928e8bd4f0add0b0afd1f3b68979f669003548b
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-07-15 12:27:05 -0400

    signInButton method implemented

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -27,6 +27,32 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-Widget _signInButton(){
-
+Widget _signInButton() {
+  return OutlineButton(
+    splashColor: Colors.grey,
+    onPressed: () {},
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    highlightElevation: 0,
+    borderSide: BorderSide(color: Colors.grey),
+    child: Padding(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(image: AssetImage('assets/google_logo.png'), height: 35.0),
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
